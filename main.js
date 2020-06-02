e91a131738d9f220169f04076dd234d873505553
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2020-06-02 09:32:45 +0200

    [IMP] - Image editing

--- main.js
+++ main.js
@@ -94,4 +94,8 @@ window.addEventListener('load', (event) => {
     }
   });
 
+  /*===============   Viewport Fix   ===============*/
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 });
